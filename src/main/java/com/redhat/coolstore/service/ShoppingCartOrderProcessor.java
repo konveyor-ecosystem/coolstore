d837bc298ad44c29d49e0791a7ef03be18d71bdd
Shawn Hurley <shawn@hurley.page>
2024-11-18 15:39:38 -0500

    Run with faster validator, probably not right
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
@@ -1,11 +1,11 @@
+
 package com.redhat.coolstore.service;
 
 import java.util.logging.Logger;
-import javax.ejb.Stateless;
-import javax.annotation.Resource;
-import javax.inject.Inject;
-import javax.jms.JMSContext;
-import javax.jms.Topic;
+import jakarta.ejb.Stateless;
+import jakarta.annotation.Inject;
+import jakarta.jms.JMSContext;
+import jakarta.jms.Topic;
 
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.utils.Transformers;
@@ -20,7 +20,7 @@ public class ShoppingCartOrderProcessor  {
     @Inject
     private transient JMSContext context;
 
-    @Resource(lookup = "java:/topic/orders")
+    @jakarta.annotation.Resource(lookup = "java:/topic/orders")
     private Topic ordersTopic;
 
     

cfb5a7f2bac49a9961aa58721a541a5f9dd8a2d1
Pranav Gaikwad <pgaikwad@redhat.com>
2025-02-13 15:21:22 -0500

    [pgaikwad] fix compilation issues

--- src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
+++ src/main/java/com/redhat/coolstore/service/ShoppingCartOrderProcessor.java
@@ -4,8 +4,8 @@ import java.util.logging.Logger;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.utils.Transformers;

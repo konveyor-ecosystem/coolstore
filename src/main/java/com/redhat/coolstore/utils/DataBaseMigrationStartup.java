c4ebd452a52eca6ea6b426ca777c926166ead3b7
Pranav Gaikwad <pgaikwad@redhat.com>
2025-02-14 09:43:06 -0500

    pgaikwad: more fixes to get the app running
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
+++ src/main/java/com/redhat/coolstore/utils/DataBaseMigrationStartup.java
@@ -32,14 +32,15 @@ public class DataBaseMigrationStartup {
     private void startup() {
         try {
             logger.info("Initializing/migrating the database using FlyWay");
-            Flyway flyway = new Flyway();
-            flyway.setDataSource(dataSource);
+            Flyway flyway = Flyway.configure()
+                                  .dataSource(dataSource) // Use the configure method to set the data source
+                                  .load();
             flyway.baseline();
             // Start the db.migration
             flyway.migrate();
         } catch (FlywayException e) {
-            if(logger !=null)
-                logger.log(Level.SEVERE,"FAILED TO INITIALIZE THE DATABASE: " + e.getMessage(),e);
+            if(logger != null)
+                logger.log(Level.SEVERE, "FAILED TO INITIALIZE THE DATABASE: " + e.getMessage(), e);
             else
                 System.out.println("FAILED TO INITIALIZE THE DATABASE: " + e.getMessage() + " and injection of logger doesn't work");
         }
